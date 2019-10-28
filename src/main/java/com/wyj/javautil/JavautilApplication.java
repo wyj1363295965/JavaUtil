0866742e3ee40386c04d8fec48935a805c327a3f
wyj <1363295965@qq.com>
2019-10-28 14:35:04 +0800

    add

---
+++ src/main/java/com/wyj/javautil/JavautilApplication.java
@@ -0,0 +1,13 @@
+package com.wyj.javautil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavautilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavautilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wyj/javautil/JavautilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyj.javautil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavautilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
